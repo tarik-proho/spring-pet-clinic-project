4b4fcf550ad7209fe1de1bbd4ca863b73789333b
tproho1 <44259164+tproho1@users.noreply.github.com>
2020-02-21 01:34:00 +0100

    init

---
+++ src/main/java/springframeworkapp/springpetclinicproject/SpringPetClinicProjectApplication.java
@@ -0,0 +1,13 @@
+package springframeworkapp.springpetclinicproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinicProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetClinicProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springframeworkapp/springpetclinicproject/SpringPetClinicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package springframeworkapp.springpetclinicproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
